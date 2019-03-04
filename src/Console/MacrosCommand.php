ef40cd66dd50e57d6e60e7986aa910984790d143
Morva Kristóf <kristof92@gmail.com>
2019-03-04 10:40:50 +0100

    Fix coding style

--- src/Console/MacrosCommand.php
+++ src/Console/MacrosCommand.php
@@ -89,7 +89,7 @@ class MacrosCommand extends Command
             $this->generateNamespace($reflection->getNamespaceName(), function () use ($macros, $reflection) {
                 $this->generateClass($reflection->getShortName(), function () use ($macros) {
                     foreach ($macros as $name => $macro) {
-                        if(is_array($macro)) {
+                        if (is_array($macro)) {
                             list($class, $method) = $macro;
                             $function = new \ReflectionMethod(is_object($class) ? get_class($class) : $class, $method);
                         } else {
